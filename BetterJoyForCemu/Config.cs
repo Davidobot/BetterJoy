238a192bec9fa75075718507d44ef9e184d7e295
Shuken <shukenmg@iuvenisstudios.ga>
2020-09-20 11:25:21 +0800

    Update Config.cs
    Fixes the part where it checks if settings are old.

--- BetterJoyForCemu/Config.cs
+++ BetterJoyForCemu/Config.cs
@@ -21,19 +21,39 @@ namespace BetterJoyForCemu {
 			return "0";
 		}
 
-		public static void Init(List<KeyValuePair<string, float[]>> caliData) {
+        // Helper function to count how many lines are in a file
+        static long CountLinesInFile(string f) {
+            // Zero based count
+            long count = -1;
+            using (StreamReader r = new StreamReader(f)) {
+                string line;
+                while ((line = r.ReadLine()) != null) {
+                    count++;
+                }
+            }
+            return count;
+        }
+
+        public static void Init(List<KeyValuePair<string, float[]>> caliData) {
 			foreach (string s in new string[] { "ProgressiveScan", "StartInTray", "capture", "home", "sl_l", "sl_r", "sr_l", "sr_r", "shake", "reset_mouse", "active_gyro" })
 				variables[s] = GetDefaultValue(s);
 
-			if (File.Exists(PATH)) {
-				int lineNO = -1;
+            if (File.Exists(PATH)) {
+
+                // Reset settings file if old settings
+                if (CountLinesInFile(PATH) < settingsNum) {
+                    File.Delete(PATH);
+                    Init(caliData);
+                    return;
+                }
+
 				using (StreamReader file = new StreamReader(PATH)) {
 					string line = String.Empty;
-
-					while ((line = file.ReadLine()) != null) {
+                    int lineNO = 0;
+                    while ((line = file.ReadLine()) != null) {
 						string[] vs = line.Split();
-						try {
-							if (lineNO < settingsNum) { // load in basic settings
+                        try {
+                            if (lineNO < settingsNum) { // load in basic settings
 								variables[vs[0]] = vs[1];
 							} else { // load in calibration presets
 								caliData.Clear();
@@ -50,16 +70,8 @@ namespace BetterJoyForCemu {
 								}
 							}
 						} catch { }
-						lineNO++;
-					}
-
-
-				}
-
-				// if old settings
-				if (lineNO < settingsNum) {
-					File.Delete(PATH);
-					Init(caliData);
+                        lineNO++;
+                    }
 				}
 			} else {
 				using (StreamWriter file = new StreamWriter(PATH)) {
