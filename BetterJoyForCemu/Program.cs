0a2127b057ae2d49a3ecb5310188b59bd27a2278
BurndiL <94790464+BurndiL@users.noreply.github.com>
2021-11-21 17:46:41 +0100

    Float Parsing issue fix

--- BetterJoyForCemu/Program.cs
+++ BetterJoyForCemu/Program.cs
@@ -511,6 +511,9 @@ namespace BetterJoyForCemu {
         private static string appGuid = "1bf709e9-c133-41df-933a-c9ff3f664c7b"; // randomly-generated
         static void Main(string[] args) {
 
+            // Setting the culturesettings so float gets parsed correctly
+            CultureInfo.CurrentCulture = new CultureInfo("en-US", false);
+            
             // Set the correct DLL for the current OS
             SetupDlls();
 
